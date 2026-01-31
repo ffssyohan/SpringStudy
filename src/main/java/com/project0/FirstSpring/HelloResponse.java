2589f2002a765a6f176ecd3a7559e5d24b1b600c
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-01-31 20:57:06 -0300

    Simple encapsulation and JSON returning methods

---
+++ src/main/java/com/project0/FirstSpring/HelloResponse.java
@@ -0,0 +1,17 @@
+package com.project0.FirstSpring;
+
+public class HelloResponse {
+    private String response;
+
+    public HelloResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
